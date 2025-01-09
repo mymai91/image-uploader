a3f31c6baf4620b11b8fa8a49f0770f6e901af34
My Mai <my.mai@we-are-mea.com>
2025-01-10 10:54:49 +1300

    Import Redux and chakra ui

---
+++ web-app/src/providers/ReduxProvider.tsx
@@ -0,0 +1,9 @@
+import { store } from '@/store'
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+
+const ReduxProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>
+}
+
+export default ReduxProvider
