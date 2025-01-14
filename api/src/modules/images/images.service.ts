60ff9ec1588f8c48c5e6db98e6f4fed342ca9ec7
My Mai <my.mai@we-are-mea.com>
2025-01-15 00:34:57 +1300

    Jwt enhancement

--- api/src/modules/images/images.service.ts
+++ api/src/modules/images/images.service.ts
@@ -9,7 +9,6 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { COMPRESS_OPTIONS } from '@/common/config/data.config';
 import { User } from '../users/entities/user.entity';
-import { UsersService } from '../users/users.service';
 import { UserUtil } from '@/common/utils/user.utils';
 
 @Injectable()
@@ -17,8 +16,6 @@ export class ImagesService {
   constructor(
     @InjectRepository(Image)
     private imageRepository: Repository<Image>,
-
-    private usersService: UsersService,
 
     private userUtils: UserUtil,
   ) {}
@@ -28,13 +25,7 @@ export class ImagesService {
     uploadImageDto: UploadImageDto,
     user: User,
   ): Promise<Image> {
-    // const currentUser = await this.usersService.findById(user.id);
-
-    // if (!currentUser) {
-    //   throw new UnauthorizedException('User not authenticated');
-    // }
-
-    const currentUser = await this.userUtils.getCurrentUser(user.id);
+    const currentUser = await this.userUtils.getCurrentUser(user.email);
 
     try {
       // Define the compressed file path
@@ -76,7 +67,7 @@ export class ImagesService {
   }
 
   async getAll(user: User): Promise<Image[]> {
-    const currentUser = await this.userUtils.getCurrentUser(user.id);
+    const currentUser = await this.userUtils.getCurrentUser(user.email);
     console.log('currentUser', currentUser);
     return await this.imageRepository.find({
       where: { user: currentUser },
