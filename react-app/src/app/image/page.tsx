42871ea94c6619903626e0d6030f17cc0687dba2
My Mai <my.mai@we-are-mea.com>
2025-01-20 18:46:30 +1300

    main page

---
+++ react-app/src/app/image/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const ImagePage = () => {
+  return (
+    <div className="min-h-screen flex">
+      <h1>ImagePage</h1>
+    </div>
+  )
+}
+
+export default ImagePage
--- react-app/src/app/login/page.tsx
+++ react-app/src/app/login/page.tsx
@@ -1,5 +1,12 @@
-const LoginPage = () => {
-  return <div className="min-h-screen flex"></div>
-}
+// src/app/login/page.tsx
+"use client"
 
-export default LoginPage
+import { LoginForm } from "./component/LoginForm"
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <LoginForm />
+    </div>
+  )
+}
