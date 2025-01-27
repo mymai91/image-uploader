b8ec6dad5cec2166075d023b488dc5b33d629d01
My Mai <my.mai@we-are-mea.com>
2025-01-27 15:23:50 +1300

    prefill angular app

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -13,9 +13,18 @@ export class AppComponent {
   constructor(private router: Router) {}
   title = 'image-uploader-angular-app'
 
+  // showHeader(): boolean {
+  //   return (
+  //     this.router.url !== '/login' &&
+  //     this.router.url !== '/register' &&
+  //     this.router.url !== '/'
+  //   )
+  // }
+
   showHeader(): boolean {
+    const urlWithoutQuery = this.router.url.split('?')[0]
     return (
-      this.router.url !== '/login' &&
+      urlWithoutQuery !== '/login' &&
       this.router.url !== '/register' &&
       this.router.url !== '/'
     )
